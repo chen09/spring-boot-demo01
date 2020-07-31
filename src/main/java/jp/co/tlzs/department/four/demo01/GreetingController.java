8486f5c62ca4204280b9433d4bcabb643e7091af
chenxin <chen09@gmail.com>
2020-08-01 02:07:58 +0900

    add param

--- src/main/java/jp/co/tlzs/department/four/demo01/GreetingController.java
+++ src/main/java/jp/co/tlzs/department/four/demo01/GreetingController.java
@@ -1,13 +1,14 @@
 package jp.co.tlzs.department.four.demo01;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
     @GetMapping("/greeting")
-    public Greeting greeting() {
-        return new Greeting(1,"aa");
+    public Greeting greeting(@RequestParam(value = "username") String name) {
+        return new Greeting(1, name);
     }
 
 }
