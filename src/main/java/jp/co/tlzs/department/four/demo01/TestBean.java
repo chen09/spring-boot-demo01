c1fd7fa70e15680a1a55a0e06bcd748d14fa7803
chenxin <chen09@gmail.com>
2020-08-02 00:24:11 +0900

    commit

---
+++ src/main/java/jp/co/tlzs/department/four/demo01/TestBean.java
@@ -0,0 +1,11 @@
+package jp.co.tlzs.department.four.demo01;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TestBean {
+    private String name;
+
+}
---
+++ src/main/java/jp/co/tlzs/department/four/demo01/TestController.java
@@ -0,0 +1,14 @@
+package jp.co.tlzs.department.four.demo01;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public TestBean test() {
+        return new TestBean("aaaa");
+    }
+
+}
