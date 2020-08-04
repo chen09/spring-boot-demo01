9c6172c75a136bc8775bf23a7c433acc8575a9a2
chenxin <chen09@gmail.com>
2020-08-04 21:57:55 +0900

    test

--- src/main/java/jp/co/tlzs/department/four/demo01/TestController.java
+++ src/main/java/jp/co/tlzs/department/four/demo01/TestController.java
@@ -1,9 +1,11 @@
 package jp.co.tlzs.department.four.demo01;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class TestController {
     @GetMapping("/test")
