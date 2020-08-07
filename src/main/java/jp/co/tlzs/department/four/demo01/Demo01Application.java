7f8d890d9107d8c0ef7e1f6ffe6fbe24c7751e65
chenxin <chen09@gmail.com>
2020-08-08 00:24:01 +0900

    update mybatis

--- src/main/java/jp/co/tlzs/department/four/demo01/Demo01Application.java
+++ src/main/java/jp/co/tlzs/department/four/demo01/Demo01Application.java
@@ -1,9 +1,12 @@
 package jp.co.tlzs.department.four.demo01;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("jp.co.tlzs.department.four.demo01.mapper")
+//@MapperScan("com.kfit.*.mapper")
 public class Demo01Application {
 
 	public static void main(String[] args) {
--- src/main/java/jp/co/tlzs/department/four/demo01/TestController.java
+++ src/main/java/jp/co/tlzs/department/four/demo01/TestController.java
@@ -1,16 +1,30 @@
 package jp.co.tlzs.department.four.demo01;
 
+import jp.co.tlzs.department.four.demo01.mapper.UserMapper;
+import jp.co.tlzs.department.four.demo01.model.User;
+import jp.co.tlzs.department.four.demo01.model.UserExample;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @CrossOrigin
 @RestController
 public class TestController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/test")
-    public TestBean test() {
-        return new TestBean("aaaa");
+    public List<User> test() {
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andIdEqualTo(1);
+        List<User> users = userMapper.selectByExample(userExample);
+
+        return users;
     }
 
 }
