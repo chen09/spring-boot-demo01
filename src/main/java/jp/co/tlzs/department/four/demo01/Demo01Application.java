717c4474ca48666357b73afcb53176df81e97884
chenxin <chen09@gmail.com>
2021-01-28 20:15:06 +0900

    test

--- src/main/java/jp/co/tlzs/department/four/demo01/Demo01Application.java
+++ src/main/java/jp/co/tlzs/department/four/demo01/Demo01Application.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo01Application {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+//		System.out.println("test");
 		SpringApplication.run(Demo01Application.class, args);
 	}
 
