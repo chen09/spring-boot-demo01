3321d75a94116784f20b0a5b9b62a0ab982710b5
chink-aucnet <chink@ns.aucnet.co.jp>
2021-01-29 18:11:04 +0900

    Update Demo01Application.java
    test

--- src/main/java/jp/co/tlzs/department/four/demo01/Demo01Application.java
+++ src/main/java/jp/co/tlzs/department/four/demo01/Demo01Application.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 @MapperScan("jp.co.tlzs.department.four.demo01.mapper")
 //@MapperScan("com.kfit.*.mapper")
